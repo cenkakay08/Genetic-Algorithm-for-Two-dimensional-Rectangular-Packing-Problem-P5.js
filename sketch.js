0583a58b90e38b63ea787344b82381d2d14fbb4a
CNEK <cenk_akay08@hotmail.com>
2021-02-19 09:44:42 +0300

    added id attribute to rectangulars for showing order number of itself

--- sketch.js
+++ sketch.js
@@ -4,17 +4,21 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   for (i = 0; i < 100; i++) {
     Rectangulars[i] = new Rectangular(
-      Math.floor(Math.random() * (100 - 5 + 1)) + 5,
-      Math.floor(Math.random() * (100 - 5 + 1)) + 5,
+      Math.floor(Math.random() * (100 - 10 + 1)) + 10,
+      Math.floor(Math.random() * (100 - 10 + 1)) + 10,
       Math.floor(Math.random() * (windowWidth - 100 - 1 + 1)) + 1,
-      Math.floor(Math.random() * (windowHeight - 100 - 1 + 1)) + 1
+      Math.floor(Math.random() * (windowHeight - 100 - 1 + 1)) + 1,
+      i
     );
   }
+  var button = createButton("bottom left");
+  button.position(windowWidth - 100, windowHeight - 100);
+  button.mousePressed(bottomLeft);
   console.log(Rectangulars);
 }
 
 function draw() {
-  background("#00203FFF");
+  background("#C6E2FF");
   for (i = 0; i < Rectangulars.length; i++) {
     Rectangulars[i].show();
   }
@@ -23,17 +27,26 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
+function bottomLeft() {
+  Rectangulars[0].x = 0;
+  Rectangulars[0].y = 0;
+}
 class Rectangular {
-  constructor(width, height, x, y) {
+  constructor(width, height, x, y, id) {
     this.width = width;
     this.height = height;
     this.x = x;
     this.y = y;
+    this.id = id;
   }
   show() {
-    stroke(255);
-    strokeWeight(1);
+    stroke(0);
+    strokeWeight(2);
     noFill();
     rect(this.x, this.y, this.width, this.height);
+    textAlign(CENTER, CENTER);
+    stroke(0);
+    strokeWeight(1);
+    text(this.id, this.x + this.width / 2, this.y + this.height / 2);
   }
 }
