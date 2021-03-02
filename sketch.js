2a87acf2c3e034c3d7e412dc552aaa8f0d82dbbb
Baran Eryürek <baran.4821@hotmail.com>
2021-03-03 00:36:14 +0300

    initial commit

---
+++ sketch.js
@@ -0,0 +1,62 @@
+let target;
+let popmax;
+let mutationRate;
+let population;
+
+let bestPhrase;
+let allPhrases;
+let stats;
+
+function setup() {
+  bestPhrase = createP("Best phrase:");
+  bestPhrase.class("best");
+
+  allPhrases = createP("All phrases:");
+  allPhrases.position(600, 10);
+  allPhrases.class("all");
+
+  stats = createP("Stats");
+  stats.class("stats");
+
+  target = [1,2,3,4,5,6];
+  popmax = 100;
+  mutationRate = 0.01;
+  let initial = [2,5,6,4,1,3];
+
+  // Create a population
+  population = new Population(target, mutationRate, popmax, initial);
+}
+
+function draw() {
+  // Generate mating pool
+  population.naturalSelection();
+  //Create next generation
+  population.generate();
+  // Calculate fitness
+  population.calcFitness();
+
+  population.evaluate();
+
+  // If we found the target stop
+  if (population.isFinished()) {
+    noLoop();
+  }
+
+  displayInfo();
+}
+
+function displayInfo() {
+  // Display current status of population
+  let answer = population.getBest();
+
+  bestPhrase.html("Best phrase:<br>" + answer);
+
+  let statstext = "total generations:     " + population.getGenerations() + "<br>";
+  statstext += "average fitness:       " + nf(population.getAverageFitness()) + "<br>";
+  statstext += "total population:      " + popmax + "<br>";
+  statstext += "mutation rate:         " + floor(mutationRate * 100) + "%";
+
+  stats.html(statstext);
+
+  allPhrases.html("All phrases:<br>" + population.allPhrases())
+}
