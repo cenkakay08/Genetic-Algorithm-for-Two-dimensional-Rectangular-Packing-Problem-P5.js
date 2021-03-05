d4386cb716a063d9ce49b3d73a7500fbac2fcd73
CNEK <cenk_akay08@hotmail.com>
2021-03-05 05:19:22 +0300

    refactoring for blf, not completed needs more improve

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 let Rectangulars = [];
-let Border_line_x_point = 500;
-let Avaliable_points = [];
+let Border_line_x_point = 300;
+let All_lines = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  grid = new Grid();
   for (i = 0; i < 100; i++) {
     Rectangulars[i] = new Rectangular(
       Math.floor(Math.random() * (100 - 10 + 1)) + 10,
@@ -12,13 +13,13 @@ function setup() {
       i
     );
   }
-  //initial point of the empty bin
-  Avaliable_points[0] = new Avaliable_point(0, windowHeight);
-  console.log(Avaliable_points);
-
+  for (i = 0; i < windowHeight; i++) {
+    All_lines[i] = new All_line(0, i, Border_line_x_point);
+  }
+  console.log(All_lines);
   var button = createButton("bottom left");
   button.position(windowWidth - 100, windowHeight - 100);
-  button.mousePressed(bottomLeft);
+  button.mousePressed(order);
   console.log(Rectangulars);
 }
 
@@ -28,41 +29,54 @@ function draw() {
     Rectangulars[i].show();
   }
 
+  strokeWeight(1);
   line(Border_line_x_point, 0, Border_line_x_point, windowHeight);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function bottomLeft() {
-  for (i = 0; i <= 10; i++) {
-    console.log(Avaliable_points);
-    Rectangulars[i].x = Avaliable_points[0].getX();
-    Rectangulars[i].y = Avaliable_points[0].getY() - Rectangulars[i].height;
+function order() {
+  var bestLine = getBestLine(Rectangulars[0].width);
+  Rectangulars[0].x = bestLine.startPoint_x;
+  Rectangulars[0].y = bestLine.startPoint_y - (Rectangulars[0].height - 1);
+  updateLines(Rectangulars[0]);
+}
+function updateLines(PlacedRectangular) {
+  var PlacedRectangularY = PlacedRectangular.y;
+  for (i = 0; i < PlacedRectangular.height; i++) {
+    for (k = 0; k < All_lines.length; k++) {
+      if (PlacedRectangularY == All_lines[k].startPoint_y) {
+        if (PlacedRectangular.x >= All_lines[k].startPoint_x) {
+          if (PlacedRectangular.x <= All_lines[k].endPoint_x) {
+            All_lines[k].startPoint_x =
+              All_lines[k].startPoint_x + PlacedRectangular.width;
+            PlacedRectangularY = PlacedRectangularY + 1;
+          }
+        }
+      }
+    }
+  }
+  console.log(All_lines);
+}
 
-    update_Avaliable_points(Avaliable_points[0], Rectangulars[i]);
+function getBestLine(widthofRectangular) {
+  All_lines.sort((a, b) => {
+    if (a.startPoint_y == b.startPoint_y) {
+      return a.startPoint_x - b.startPoint_x;
+    }
+    return b.startPoint_y - a.startPoint_y;
+  });
+  for (i = 0; i < All_lines.length; i++) {
+    if (
+      widthofRectangular <=
+      All_lines[i].endPoint_x - All_lines[i].startPoint_x
+    ) {
+      console.log(All_lines[i]);
+      return All_lines[i];
+    }
   }
 }
-
-function update_Avaliable_points(filled_point, filled_rectangular) {
-  /*   var newPoint1 = new Avaliable_point(
-    filled_point.getX(),
-    filled_point.getY() - filled_rectangular.height
-  );
-  Avaliable_points.push(newPoint1); */
-  var newPoint2 = new Avaliable_point(
-    filled_point.getX() + filled_rectangular.width,
-    filled_point.getY()
-  );
-
-  Avaliable_points.push(newPoint2);
-  Avaliable_points = Avaliable_points.filter(
-    (element) => element !== filled_point
-  );
-
-  console.log(Avaliable_points);
-}
-function getBestPoint() {}
 
 class Rectangular {
   constructor(width, height, x, y, id) {
@@ -83,19 +97,17 @@ class Rectangular {
     text(this.id, this.x + this.width / 2, this.y + this.height / 2);
   }
 }
-class Avaliable_point {
-  constructor(startPoint_x, startPoint_y, empty = true) {
+class All_line {
+  constructor(startPoint_x, startPoint_y, endPoint_x) {
     this.startPoint_x = startPoint_x;
     this.startPoint_y = startPoint_y;
-    this.empty = empty;
+    this.endPoint_x = endPoint_x;
   }
+  show() {}
   getY() {
     return this.startPoint_y;
   }
   getX() {
     return this.startPoint_x;
-  }
-  setEmptyTrue() {
-    this.empty = true;
   }
 }
