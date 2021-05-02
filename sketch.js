cc710bc5a6b7dd4a4db369e86ef88bc1cb7a059e
Baran Eryürek <baran.4821@hotmail.com>
2021-05-02 14:08:18 +0300

    asd

--- sketch.js
+++ sketch.js
@@ -1,33 +1,48 @@
-let target;
-let popmax;
-let mutationRate;
-let population;
+// EmptY arraY for Rectangulars.
+let Rectangulars = [];
 
-let bestPhrase;
-let allPhrases;
-let stats;
+// Bin corner border value.
+let Border_line_X = 800;
+
+// Set number for quantity of random Rectangulars.
+let Number_for_random_rectangulars = 150;
 
 function setup() {
-  bestPhrase = createP("Best phrase:");
-  bestPhrase.class("best");
+  // Canvas created according to Window dimensions.
+  createCanvas(windowWidth, windowHeight);
+  // Create Rectangulars that has random height and width size.
+  for (i = 0; i < Number_for_random_rectangulars; i++) {
+    Rectangulars[i] = new Rectangular(
+      Math.floor(Math.random() * (100 - 10 + 1)) + 10,
+      Math.floor(Math.random() * (100 - 10 + 1)) + 10,
+      Math.floor(Math.random() * (windowWidth - 100 - 1 + 1)) + 1,
+      Math.floor(Math.random() * (windowHeight - 100 - 1 + 1)) + 1,
+      i
+    );
+  }
 
-  allPhrases = createP("All phrases:");
-  allPhrases.position(600, 10);
-  allPhrases.class("all");
-
-  stats = createP("Stats");
-  stats.class("stats");
-
-  target = [1,2,3,4,5,6];
+  target = 10;
   popmax = 100;
   mutationRate = 0.01;
   let initial = [2,5,6,4,1,3];
 
-  // Create a population
+  // Create a population with a target phrase, mutation rate, and population max
   population = new Population(target, mutationRate, popmax, initial);
 }
 
 function draw() {
+  // Background color
+  background("#E2F0FF");
+
+  // Draw the all Rectangulars
+  for (i = 0; i < Rectangulars.length; i++) {
+    Rectangulars[i].show();
+  }
+
+  // Draw corner border line
+  strokeWeight(1);
+  line(Border_line_X, 0, Border_line_X, windowHeight);
+
   // Generate mating pool
   population.naturalSelection();
   //Create next generation
@@ -37,26 +52,184 @@ function draw() {
 
   population.evaluate();
 
-  // If we found the target stop
+  // If we found the target phrase, stop
   if (population.isFinished()) {
     noLoop();
   }
-
-  displayInfo();
 }
 
-function displayInfo() {
-  // Display current status of population
-  let answer = population.getBest();
+// Function for resize Canvas according to Window dimensions.
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
 
-  bestPhrase.html("Best phrase:<br>" + answer);
+function NewBottomLeftFunction(RectangelArrays, BinWidth, BinHeight) {
+  var Lines = [];
+  for (i = 0; i < BinHeight; i++) {
+    Lines[i] = new All_line(0, BinWidth, i);
+  }
+  Lines = NewSortLines(Lines);
+  console.log(RectangelArrays.length);
+  for (z = 0; z < RectangelArrays.length; z++) {
+    var bestLine = NewGetBestLine(RectangelArrays[z], Lines);
+    RectangelArrays[z].X = bestLine.Start_point_X;
+    RectangelArrays[z].Y = bestLine.Y - RectangelArrays[z].height;
+    Lines = NewUpdateLines(RectangelArrays[z], Lines);
+    Lines = NewSortLines(Lines);
+  }
+  Rectangulars = RectangelArrays;
+  var Score = NewGetScore(RectangelArrays);
+  return Score;
+}
+function NewGetScore(RectangelArrays) {
+  var ratio = parseInt((RectangelArrays.length / 100) * 20);
+  var Score = 9999;
+  for (i = RectangelArrays.length - ratio; i < RectangelArrays.length; i++) {
+    var tempScore = RectangelArrays[i].Y;
+    if (tempScore < Score) {
+      Score = tempScore;
+    }
+  }
+  console.log(Score);
+  console.log(RectangelArrays);
+  return Score;
+}
 
-  let statstext = "total generations:     " + population.getGenerations() + "<br>";
-  statstext += "average fitness:       " + nf(population.getAverageFitness()) + "<br>";
-  statstext += "total population:      " + popmax + "<br>";
-  statstext += "mutation rate:         " + floor(mutationRate * 100) + "%";
+function NewGetBestLine(Selected_Rect, Lines) {
+  for (i = 0; i < Lines.length; i++) {
+    if (Selected_Rect.width <= Lines[i].End_point_X - Lines[i].Start_point_X) {
+      //console.log(JSON.parse(JSON.stringify(Used_best_lines)));
+      return Lines[i];
+    }
+  }
+}
+function NewSortLines(Lines) {
+  var SortedLines = Lines.sort((a, b) => {
+    if (a.Y == b.Y) {
+      return a.Start_point_X - b.Start_point_X;
+    }
+    return b.Y - a.Y;
+  });
+  return SortedLines;
+}
 
-  stats.html(statstext);
+function NewUpdateLines(PlacedRectangular, Lines) {
+  var PlacedRectangularY = PlacedRectangular.Y;
 
-  allPhrases.html("All phrases:<br>" + population.allPhrases())
+  for (i = 0; i < PlacedRectangular.height + 1; i++) {
+    for (k = 0; k < Lines.length; k++) {
+      // Aynı y sırasında olanlar
+      if (PlacedRectangularY == Lines[k].Y) {
+        // Durum 1 tam oturursa
+        if (
+          PlacedRectangular.X == Lines[k].Start_point_X &&
+          PlacedRectangular.X + PlacedRectangular.width == Lines[k].End_point_X
+        ) {
+          Lines[k].Y = -100;
+          PlacedRectangularY = PlacedRectangularY + 1;
+          // Druum 2 sol tarafı kapalı sağ açık
+        } else if (
+          PlacedRectangular.X == Lines[k].Start_point_X &&
+          PlacedRectangular.X + PlacedRectangular.width < Lines[k].End_point_X
+        ) {
+          Lines[k].Start_point_X =
+            PlacedRectangular.X + PlacedRectangular.width;
+          PlacedRectangularY = PlacedRectangularY + 1;
+          // durum 3 sağ tarafı kapalı sol açık
+        } else if (
+          PlacedRectangular.X > Lines[k].Start_point_X &&
+          PlacedRectangular.X + PlacedRectangular.width == Lines[k].End_point_X
+        ) {
+          Lines[k].End_point_X = PlacedRectangular.X;
+          PlacedRectangularY = PlacedRectangularY + 1;
+          // durum 4 ortada her tarafı açıkta
+        } else if (
+          PlacedRectangular.X > Lines[k].Start_point_X &&
+          PlacedRectangular.X + PlacedRectangular.width < Lines[k].End_point_X
+        ) {
+          temp_End_point_X = Lines[k].End_point_X;
+          Lines[k].End_point_X = PlacedRectangular.X;
+          var CuttedLine = new All_line(
+            PlacedRectangular.X + PlacedRectangular.width,
+            temp_End_point_X,
+            Lines[k].Y
+          );
+          Lines.push(CuttedLine);
+          PlacedRectangularY = PlacedRectangularY + 1;
+        }
+      }
+    }
+  }
+  Lines = NewDeleteUnderLines(PlacedRectangular, Lines);
+  return Lines;
+}
+
+function NewDeleteUnderLines(PlacedRectangular, Lines) {
+  for (t = 0; t < PlacedRectangular.width + 1; t++) {
+    for (f = 0; f < Lines.length; f++) {
+      if (PlacedRectangular.Y + PlacedRectangular.height < Lines[f].Y) {
+        // Durum 1 eğer line başlangııc aynı x de ise
+        if (PlacedRectangular.X == Lines[f].Start_point_X) {
+          //Durum 1.1 bitiş noktası dikdörtgenin dışında  ise
+          if (
+            PlacedRectangular.X + PlacedRectangular.width <
+            Lines[f].Start_point_X
+          ) {
+            Lines[f].Start_point_X =
+              PlacedRectangular.X + PlacedRectangular.width;
+          }
+          //Durum 1.2 bitiş noktası içinde veya bitişte
+          else {
+            Lines[f].Y = -10;
+          }
+        }
+        // Durum 2 başlangıç dikdörtgenin içinde ise
+        else if (
+          Lines[f].Start_point_X > PlacedRectangular.X &&
+          Lines[f].Start_point_X < PlacedRectangular.X + PlacedRectangular.width
+        ) {
+          //Durum 2.1 bitiş noktası dikdörtgenin dışında  ise
+          if (
+            PlacedRectangular.X + PlacedRectangular.width <
+            Lines[f].Start_point_X
+          ) {
+            Lines[f].Start_point_X =
+              PlacedRectangular.X + PlacedRectangular.width;
+          }
+          //Durum 2.2 bitiş noktası içinde veya bitişte
+          else {
+            Lines[f].Y = -10;
+          }
+        }
+      }
+    }
+  }
+  return Lines;
+}
+
+class Rectangular {
+  constructor(width, height, X, Y, id) {
+    this.width = width;
+    this.height = height;
+    this.X = X;
+    this.Y = Y;
+    this.id = id;
+  }
+  show() {
+    stroke(0);
+    strokeWeight(1);
+    noFill();
+    rect(this.X, this.Y, this.width, this.height);
+    textAlign(CENTER, CENTER);
+    stroke(0);
+    strokeWeight(1);
+    text(this.id, this.X + this.width / 2, this.Y + this.height / 2);
+  }
+}
+class All_line {
+  constructor(Start_point_X, End_point_X, Y) {
+    this.Start_point_X = Start_point_X;
+    this.End_point_X = End_point_X;
+    this.Y = Y;
+  }
 }
